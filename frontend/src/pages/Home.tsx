fc4d84eb612485c7e4834aaf83e78df93676d016
Samrudh Kishsan P M <126341963+k-ish-san@users.noreply.github.com>
2025-06-02 15:27:28 +0000

    fix: update theme handling in Home component for improved visual effects

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,59 +1,46 @@
-// import Typewriter from "typewriter-effect";
+
 import Waves from "../components/Waves";
 import Orb from "../components/Orb";
 
 
 
 
-function Home() {
+function Home()
+{
   return (
     <div
       className="relative flex flex-col items-center justify-center h-screen light:bg-gray-100 dark:bg-black overflow-hidden "
       style={{ zIndex: -1 }}
     >
-      {
-        localStorage.getItem("theme") === "light" ? (
-        <div style={{ width: "100%", height: "600px", position: "relative" }}>
-          <Orb
-            hoverIntensity={0}
-            rotateOnHover={true}
-            hue={0}
-            forceHoverState={true}
-          />
-        </div>
-      ) : (
+      {localStorage.getItem("theme") === "dark" ? (
+        < div style={{ width: "100%", height: "100%", position: "relative" }}>
+      <Orb
+        hoverIntensity={5}
+        rotateOnHover={false}
+        hue={40}
+        forceHoverState={true}
+      />
+    </div>
+  ) : (
+
+    < div className = "absolute inset-0 pointer-events-none" >
+      <Waves
+        lineColor="rgba(255, 255, 255, 0.5)"
+        backgroundColor="rgba(255, 255, 255, 0.2)"
+        waveSpeedX={0.02}
+        waveSpeedY={0.01}
+        waveAmpX={40}
+        waveAmpY={20}
+        friction={0.9}
+        tension={0.01}
+        maxCursorMove={120}
+        xGap={12}
+        yGap={36}
+      />
+    </div>
+  )
+}
         
-        <div className="absolute inset-0 pointer-events-none">
-          <Waves
-            lineColor="violet"
-            backgroundColor="rgba(255, 255, 255, 0.2)"
-            waveSpeedX={0.02}
-            waveSpeedY={0.01}
-            waveAmpX={40}
-            waveAmpY={20}
-            friction={0.9}
-            tension={0.01}
-            maxCursorMove={120}
-            xGap={12}
-            yGap={36}
-          />
-          </div>
-        )
-      }
-      {/* <div className="text-5xl font-bold dark:text-white light:text-gray-800 mb-4">
-        <Typewriter
-          options={{
-            strings: [
-              "Software Developer",
-              "Frontend Developer",
-              "MERN Stack Developer",
-            ],
-            autoStart: true,
-            loop: true,
-            deleteSpeed: 50,
-          }}
-        />
-      </div> */}
     </div>
   );
 }
